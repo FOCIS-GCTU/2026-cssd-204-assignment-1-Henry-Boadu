23b5a0e973614e4ba23fad7304abadda965fac4b
Henry Boadu Nana Osei <boaduhenry44@gmail.com>
2026-05-30 21:13:51 +0000

    initials.py
    File: initials.py
     Description: Print my initials HBNO in large stylized letters.
     Assignment Number: 1
    
    
     Name: Henry Boadu Nana Osei
     STUDENT ID: 2425400777
     Email: 2425400777@live.gctu.edu.gh
     Grader: Augustus Buckman
    On my honor, Henry Boadu Nana Osei, this programming assignment is my own work and I have not provided this code to any other student.

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# File: initials.py
+# Description: Print my initials HBNO in large stylized letters.
+# Assignment Number: 1
+
+
+# Name: Henry Boadu Nana Osei
+# STUDENT ID: 2425400777
+# Email: 2425400777@live.gctu.edu.gh
+# Grader: Augustus Buckman
+
+
+# On my honor, Henry Boadu Nana Osei, this programming assignment is my own work and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+  print()
+print("..HBNO")
+print()
+print("...HHH......HHH........BBBBBBBBBBB.........NNN......NNN........OOOOOOOOOOOO.....")
+print("...HHH......HHH........BBB......BBB........NNN......NNN........OOO......OOO.....")
+print("...HHH......HHH........BBB......BBB........NNNN.....NNN........OOO......OOO.....")
+print("...HHH......HHH........BBB......BBB........NNNNN....NNN........OOO......OOO.....")
+print("...HHHHHHHHHHHH........BBBBBBBBBBBB........NNN.NN...NNN........OOO......OOO.....")
+print("...HHH......HHH........BBB......BBB........NNN..NN..NNN........OOO......OOO.....")
+print("...HHH......HHH........BBB......BBB........NNN....NNNNN........OOO......OOO.....")
+print("...HHH......HHH........BBB......BBB........NNN.....NNNN........OOO......OOO.....")
+print("...HHH......HHH...**...BBBBBBBBBBBB...**...NNN......NNN...**...OOO......OOO...**")
+print("...HHH......HHH...**...BBBBBBBBBBB....**...NNN......NNN...**...OOOOOOOOOOOO...**")
+print()
 
 main()
